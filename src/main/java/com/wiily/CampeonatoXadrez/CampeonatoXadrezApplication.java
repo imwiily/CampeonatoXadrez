a3e4e2a5ab0393b60c2eb1bf80826c674c594e82
imwiily <ezebesen@gmail.com>
2025-05-27 11:37:56 -0300

    git init

---
+++ src/main/java/com/wiily/CampeonatoXadrez/CampeonatoXadrezApplication.java
@@ -0,0 +1,13 @@
+package com.wiily.CampeonatoXadrez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampeonatoXadrezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampeonatoXadrezApplication.class, args);
+	}
+
+}
